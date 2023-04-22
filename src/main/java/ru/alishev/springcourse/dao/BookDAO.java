e1ef91edbf1b009b9a5778e10f899056160a90cf
Alex <chekurov@Alexs-MacBook-Pro.local>
2023-04-22 15:49:05 +0300

    Initial commit

---
+++ src/main/java/ru/alishev/springcourse/dao/BookDAO.java
@@ -0,0 +1,11 @@
+package ru.alishev.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Alex Chekurov
+ */
+@Component
+public class BookDAO {
+    // Здесь будут лежать специфические запросы к БД (с помощью SQL)
+}
---
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -0,0 +1,11 @@
+package ru.alishev.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Alex Chekurov
+ */
+@Component
+public class PersonDAO {
+    // Здесь будут лежать специфические запросы к БД (с помощью SQL)
+}
